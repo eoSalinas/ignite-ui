c225627ddcda1f4d669b6029b559cf76b731dabd
Salinas <matheussalinas3@gmail.com>
2022-12-22 19:24:23 -0300

    Ignite UI: Until last class

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,43 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae molestiae unde temporibus repellendus aliquid dolorum quasi ad! Accusamus ipsam id temporibus iusto eveniet? Sit magnam reprehenderit officiis fugiat sunt quos.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'md',
+        'sm',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
---
+++ packages/docs/vite.config.js
@@ -0,0 +1,8 @@
+/// <reference types types="vite/client" />
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+})
